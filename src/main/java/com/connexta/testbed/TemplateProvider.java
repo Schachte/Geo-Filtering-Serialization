703654de8fb3dea3ae64d223c88c4b54e8502373
Ryan Schachte <ryan.schachte@connexta.com>
2018-02-19 14:03:05 -0700

    testbed update

--- src/main/java/com/connexta/testbed/TemplateProvider.java
+++ src/main/java/com/connexta/testbed/TemplateProvider.java
@@ -1,10 +1,13 @@
 package com.connexta.testbed;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.xml.namespace.QName;
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.filter.FunctionFactory;
 import org.geotools.filter.text.cql2.CQL;
+import org.geotools.filter.text.ecql.ECQL;
 import org.geotools.filter.v2_0.FESConfiguration;
 import org.geotools.filter.visitor.DuplicatingFilterVisitor;
 import org.geotools.xml.Encoder;
@@ -17,28 +20,21 @@ public class TemplateProvider {
   public static void main(String[] args) throws Exception {
 
     /**
-     {
-         attributeValues: [
-             {
-             name: "beNumber",
-             default: null
-             },
-             {
-             name: "cloudCoverage",
-             default: 0.98
-             }
-         ],
-         predicateTree: "(attName >= replacementFunc() AND title LIKE 'greetings') AND attName2 = replacementFunc()"
-     }
-     **/
+     * { attributeValues: [ { name: "beNumber", default: null }, { name: "cloudCoverage", default:
+     * 0.98 } ], predicateTree: "(attName >= replacementFunc() AND title LIKE 'greetings') AND
+     * attName2 = replacementFunc()" }
+     */
 
     /**
-     * This gets stripped off the incoming JSON blob
-     * Anything that contains "replacementFunc()" will get persisted as a valid XML function
-     **/
-
+     * This gets stripped off the incoming JSON blob Anything that contains "replacementFunc()" will
+     * get persisted as a valid XML function
+     */
     String CQL_TEMPLATE =
-        "(attName >= replacementFunc() AND title LIKE 'greetings') AND attName2 = replacementFunc()";
+        "(attName >= replacementFunc() AND title like 'test') AND attName2 = replacementFunc()";
+
+    /**
+     * The problem is that the replacement function
+     */
 
     /**
      * When we get the user input, preprocess all blank to pass in the name of the function This
@@ -47,10 +43,10 @@ public class TemplateProvider {
     Filter filter = CQL.toFilter(CQL_TEMPLATE);
 
     // Setup a substitution visitor that can traverse through the tree
-    FilterVisitor visitor = new SubstitutingVisitor();
+    SubstitutingVisitor visitor = new SubstitutingVisitor();
 
     // This will actually traverse through the tree
-    Filter subd = (Filter) visitor.visit((And) filter, null);
+    Filter subd = (Filter) visitor.visit(visitor, filter);
 
     Encoder encoder = setupEncoder();
 
@@ -63,12 +59,12 @@ public class TemplateProvider {
      */
     filterToXML(xml);
     filterToCQl(subd);
+    replacePlaceholderParams(CQL.toCQL(subd));
   }
 
   // This is how the data is going to be persisted following XML standards compliant spec
   private static void filterToXML(String xml) throws InterruptedException {
     print(xml);
-    printSeparator();
     TimeUnit.MILLISECONDS.sleep(1000);
   }
 
@@ -99,12 +95,6 @@ public class TemplateProvider {
     return functionFactory;
   }
 
-  private static void printSeparator() {
-    print(
-        "____________________________________________________________________"
-            + System.lineSeparator());
-  }
-
   private static void print(String msg) {
     System.out.println(msg);
   }
@@ -117,5 +107,39 @@ public class TemplateProvider {
       }
       return CommonFactoryFinder.getFilterFactory2().literal(function.evaluate(data));
     }
+
+    public Object visit(FilterVisitor visitor, Filter filter) throws Exception {
+      if (filter instanceof And) {
+        return visitor.visit((And) filter, null);
+      } else if (filter instanceof Or) {
+        return visitor.visit((Or) filter, null);
+      } else {
+        throw new Exception("Unsupported operation");
+      }
+    }
+  }
+
+  private static void replacePlaceholderParams(String postProcessCQL) {
+    ArrayList<String> userInput = new ArrayList<String>();
+
+    userInput.add("5");
+
+    userInput.add("5");
+
+    StringBuilder postUserInput = new StringBuilder();
+
+    print(String.format("The input string is: \n %s", postProcessCQL));
+
+    String[] inputCQLList = postProcessCQL.split(" ");
+    for (String token : inputCQLList) {
+      if (token.contains("replacement")) {
+        postUserInput.append(userInput.get(0) + " ");
+        userInput.remove(0);
+      } else {
+        postUserInput.append(token + " ");
+      }
+    }
+
+    print(String.format("The output string is: \n %s", postUserInput.toString()));
   }
 }
